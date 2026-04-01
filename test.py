9079f26c94e1ca2ceab777926a698e08ba4fe113
Maviya <maviyabubere84@gmail.com>
2026-04-01 07:41:13 +0530

    First Commit

---
+++ test.py
@@ -0,0 +1,128 @@
+print("Assalam Walekum, Liyari")
+
+print("Assalam Walekum, Liyari")
+
+#%%
+print("hi")
+# %%
+name="sp" 
+name_="sp"
+print(name)
+print(name_)
+print(name=name_)
+# %%
+if age>38:
+    print("age is more")
+
+# %%
+total=1+2+3+4+5+6+7+\
+4+5+6
+
+print(total)
+# %%
+a=32
+b=42
+a==b
+
+
+#Day 2
+
+# %%
+#printing variables
+a=100
+age=23
+height=6.1
+name="Maviya"
+is_student=True
+print("My age is",age)
+print("My height is",height)
+print("My name is",name)
+print("Is he a Student(T/F)",is_student)
+
+# %%
+a=100
+age=23
+height=6.1
+name="Maviya"
+is_student=True
+print(type(age))
+print(type(height))
+print(type(name))
+print(type(is_student))
+# %%
+#type casting
+float(age)
+print("hii")
+print(float(age))
+# %%
+int(height)
+# %%
+#dynamic typing
+var=10
+print(var,type(var))
+var="Hello"
+print(var,type(var))
+# %%
+age=input("Enter your age")
+print("Your age is",age,"yrs")
+
+
+# %%
+score=input("Enter your score:")
+print("My highest Score in maths is", score,"marks")
+# %%
+a=input("Enter your maths score")
+b=input("Enter your ss score")
+c=a+b
+print("your sum of score is",c)
+
+# %%
+a=int(input("Enter your maths score"))
+b=int(input("Enter your ss score"))
+c=a+b
+print("your sum of score is",c)
+# %%
+
+print("your sum of score is",a+b)
+# %%
+a=int(input("Enter your First no."))
+b=int(input("Enter your Second no."))
+print("your Frist no. is",a)
+print("your Second number is",b)
+print("your sum of score is",a+b)
+print("your sub of number is",a-b)
+print("your Product of number is",a*b)
+print("your Division of number is",a/b)
+print("your Float of number is",a//b)
+print("your Mod of number is",a%b)
+
+# %%
+a=float(input("Enter your First no."))
+b=float(input("Enter your Second no."))
+print("your Frist no. is",a)
+print("your Second number is",b)
+print("your sum of score is",a+b)
+print("your sub of number is",a-b)
+print("your Product of number is",a*b)
+print("your Division of number is",a/b)
+print("your Float of number is",a//b)
+print("your Mod of number is",a%b)
+print("Your first no. is greater",a>b)
+print("Your both no. is equal",a==b)
+print("Your second no. is greater",b>a)
+
+
+# %%
+a=int(input("Enter your age:"))
+
+
+if a>=18:
+    print("since your age is",a, "you are eligible for voting")
+elif a>=15 and a<18:
+    print("since your age is",a,"you will be eligible soon keep waiting")
+
+else:
+    print("since your age is",a,"you are not eligible for voting")
+
+#%%
+
